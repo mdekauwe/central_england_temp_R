a4b8c4eaef3d3cdf98fc12c45536f990cad3cd3e
mdekauwe <mdekauwe@gmail.com>
2016-04-30 09:47:16 +1000

    First commit

---
+++ plot_data.R
@@ -0,0 +1,23 @@
+#!/usr/bin/Rscript
+
+library("ggplot2")
+require(mgcv)
+
+url <- "http://www.metoffice.gov.uk/hadobs/hadcet/cetml1659on.dat"
+cet <- read.table(url, sep = "", skip=6, header=TRUE, fill=TRUE,
+                  na.string = c(-99.99, -99.9))
+
+# Drop 2016 as it isn't finished
+cet <- cet[-nrow(cet),]
+
+# Last column is the annual value
+years <- as.numeric(rownames(cet))
+df_ann <- data.frame(temp=cet[,ncol(cet)], year=years)
+
+ggplot(df_ann, aes(years, temp)) +
+       geom_point(size=0.8) +
+       xlab("Year") +
+       ylab("Mean annual temperature (deg C)")
+
+# Drop year from monthly data
+cet <- cet[,-ncol(cet)]
