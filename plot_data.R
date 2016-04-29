8288666a9becfe7bd839b79da7418e2517164795
mdekauwe <mdekauwe@gmail.com>
2016-04-30 09:54:44 +1000

    Fixed ratio

--- plot_data.R
+++ plot_data.R
@@ -3,6 +3,8 @@
 library("ggplot2")
 require(mgcv)
 
+golden_ratio <- 1.0 / 1.6180339887
+
 url <- "http://www.metoffice.gov.uk/hadobs/hadcet/cetml1659on.dat"
 cet <- read.table(url, sep = "", skip=6, header=TRUE, fill=TRUE,
                   na.string = c(-99.99, -99.9))
@@ -17,7 +19,8 @@ df_ann <- data.frame(temp=cet[,ncol(cet)], year=years)
 ggplot(df_ann, aes(years, temp)) +
        geom_point(size=0.8) +
        xlab("Year") +
-       ylab("Mean annual temperature (deg C)")
-
+       ylab("Mean annual temperature (deg C)") +
+       theme(aspect.ratio=golden_ratio)
+       
 # Drop year from monthly data
 cet <- cet[,-ncol(cet)]
